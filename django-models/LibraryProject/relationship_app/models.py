7c2b4569a6b17bb8f34fa1ed3ed2bef3f56a660c
ELIUZ <osongoeliuz14@gmail.com>
2025-02-19 10:46:37 +0300

    added __str__ method to each model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,21 +1,30 @@
 from django.db import models
 
-# Create your models here.
-
-
-#advanced Model Relationships 
+# Advanced Model Relationships 
 
 class Author(models.Model):
-    name = models.CharField(("Name"), max_length=200)
+    name = models.CharField("Name", max_length=200)
+
+    def __str__(self):
+        return self.name  # Returns the name of the author
 
 class Book(models.Model):
-    title = models.CharField(("Title"), max_length=200)
+    title = models.CharField("Title", max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title  # Returns the title of the book
 
 class Library(models.Model):
     name = models.CharField(max_length=200)
-    book = models.ManyToManyField("relationship_app.Book", verbose_name=("Book"))
+    book = models.ManyToManyField("relationship_app.Book", verbose_name="Book")
+
+    def __str__(self):
+        return self.name  # Returns the name of the library
 
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name  # Returns the name of the librarian
