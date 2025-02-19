6a5fb09e2d2abe0692385628703c5e4804d89409
ELIUZ <osongoeliuz14@gmail.com>
2025-02-19 10:36:25 +0300

    create models under relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+#advanced Model Relationships 
+
+class Author(models.Model):
+    name = models.CharField(("Name"), max_length=200)
+
+class Book(models.Model):
+    title = models.CharField(("Title"), max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    book = models.ManyToManyField("relationship_app.Book", verbose_name=("Book"))
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
