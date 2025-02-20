3b6e80771f3222f117517b258f0054181a116f2c
ELIUZ <osongoeliuz14@gmail.com>
2025-02-20 07:54:03 +0300

    part 2

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from relationship_app import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  # Homepage view
     path('admin/', admin.site.urls),
+    path('books/', views.list_books, name='list_books'), # FBV for listing books
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV for library details
+    
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse  # Import HttpResponse
+from django.views.generic import DetailView
+from relationship_app.models import Book, Library
 
-# Create your views here.
+# Function-based view to list all books
+
+
+def list_books(request):
+    books = Book.objects.all() # Query all books from the database
+
+    #Render the 'list_books.html' template and pass the books as context
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+def home(request):
+    return HttpResponse("Welcome to the Library Homepage!")
+
+# Class-based view (CBV) to display details for a specific library
+class LibraryDetailView(DetailView):
+    model = Library  # This CBV will work with the Library model
+    template_name = 'library_detail.html'  # Template to render the library details
+    context_object_name = 'library'  # The context variable for the template
+    
+    # Optionally, you can override get_context_data to add extra context to the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # Get the default context data
+        return context
