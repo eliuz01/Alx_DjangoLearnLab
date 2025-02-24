d95278ef7ca34e382f2cc2b45f083f522953448d
ELIUZ <osongoeliuz14@gmail.com>
2025-02-24 22:04:47 +0300

    authentification

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from relationship_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.list_books, name='list_books'), # FBV for listing books
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV for library details
-    
+    path('accounts/', include('relationship_app.urls')),  # Include the authentication URLs from relationship_app
 ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV for library details
     path('login/', views.user_login, name='login'),  # URL for login page
     path('logout/', views.user_logout, name='logout'),  # URL for logout page
-    path('register/', views.user_register, name='register'),  # URL for registration page
+    path('register/', views.register, name='register'),  # URL for registration page
 ]
