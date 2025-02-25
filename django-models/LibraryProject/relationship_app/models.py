73817c9fd653cecb3f4b81b44366eb79ea6a86e3
ELIUZ <osongoeliuz14@gmail.com>
2025-02-25 21:11:19 +0300

    change

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -33,12 +33,12 @@ class Librarian(models.Model):
 class UserProfile(models.Model):
 # Define the roles
     ADMIN = 'Admin'
-    Librarians = 'Librarians'
+    LIBRARIAN = 'Librarian'
     MEMBER = 'Member'
 
     ROLE_CHOICES = [
         (ADMIN, 'Admin'),
-        (Librarians, 'Librarians'),
+        (LIBRARIAN, 'Librarian'),
         (MEMBER, 'Member'),
     ]
 
