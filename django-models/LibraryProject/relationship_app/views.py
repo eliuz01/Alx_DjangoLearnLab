dcc897724dcf7f6b8cbb89299b9b71112e35ba33
ELIUZ <osongoeliuz14@gmail.com>
2025-02-25 22:35:55 +0300

    Librarian

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -83,7 +83,6 @@ def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 # View for Librarians
-@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
