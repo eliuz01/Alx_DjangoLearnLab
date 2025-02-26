f8b4953af04fd034977bfc703d1c381efafdfad5
ELIUZ <osongoeliuz14@gmail.com>
2025-02-26 15:22:12 +0300

    part3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from relationship_app import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, librarian_view
 
 
 
@@ -13,6 +13,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='login.html'), name='logout'),  # LogoutView, no custom template needed
     path('register/', views.register, name='register'),  # URL for registration page
     path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian-view/', librarian_view, name='librarian_view'),
     path('librarian_view/', views.librarian_view, name='librarian_view'),
     path('member_view/', views.member_view, name='member_view'),
     path('add_book/', views.add_book, name='add_book'),  # URL for adding a book
