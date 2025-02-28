4eecdc067f45a9f966cee714f8d83a38f20fb5c4
ELIUZ <osongoeliuz14@gmail.com>
2025-02-28 22:03:29 +0300

    created customuser under bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
+
  
 
 # Create your models here.
@@ -6,3 +8,10 @@ class Book(models.Model):
     title = models.CharField(max_length=200, null=False)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField() 
+
+
+# Create your models here.
+#uses inheritance  
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(("Date of Birth"), auto_now=False, auto_now_add=False, null=False)
+    profile_photo = models.ImageField(("Profile Photo"), upload_to=None, height_field=None, width_field=None, max_length=100, null=True)
