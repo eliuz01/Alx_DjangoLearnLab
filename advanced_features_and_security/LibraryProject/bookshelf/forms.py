192a3f9b96443c27be79de0fb5e8fdc9e033506c
ELIUZ <osongoeliuz14@gmail.com>
2025-03-02 19:44:48 +0300

    form.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,3 +1,19 @@
 from django import forms
-from .models import Book
+
+# forms.py
+from django import forms
+
+# Example of a basic form
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
+
+    # Optional: Custom validation method for name field
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if not name:
+            raise forms.ValidationError('Name is required.')
+        return name
+
 
