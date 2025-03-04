e9fe7f5338e87cec7f0572f4ee16fc0e3f972bb0
ELIUZ <osongoeliuz14@gmail.com>
2025-03-04 06:19:56 +0300

    title

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    titie  = models.CharField(("Title"), max_length=200)
+    title  = models.CharField(("Title"), max_length=200)
     author  = models.CharField(("Author"), max_length=100)
     published_date = models.DateField(("Published Date"))
