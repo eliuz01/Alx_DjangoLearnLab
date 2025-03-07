eb88ee84e009d79cca1ad57e61443377be8e9c13
ELIUZ <osongoeliuz14@gmail.com>
2025-03-07 07:02:07 +0300

    setting up router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookList, BookViewSet, AnotherBookViewSet
+from rest_framework import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'book_all', BookViewSet, basename='book_all')
+router.register(r'another-models', AnotherBookViewSet)
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+    path('', include(router.urls)), # This includes all routes registered with the router
 ]
