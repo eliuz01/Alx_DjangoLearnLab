28bfa7f6bdbb77b6313aa266d33964e85bd20239
ELIUZ <osongoeliuz14@gmail.com>
2025-03-12 13:01:48 +0300

    authentificatio

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import BookList, BookViewSet
+from .views import BookList, BookViewSet, MyApiView
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='books_all')
@@ -8,4 +9,6 @@ router.register(r'books_all', BookViewSet, basename='books_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
     path('', include(router.urls)), # This includes all routes registered with the router
+    path('myapi/', MyApiView.as_view(), name='my-api'),
+    path('api/token/', obtain_auth_token(), name='api-token'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets, generics
 from .models import Book
-from .serializers import BookSerializer 
+from .serializers import BookSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView 
+from rest_framework.response import Response
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -18,5 +22,16 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+class MyApiView(ApiView):
+    authentication_classes =  [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({'message': 'Hello, athenticated user!'})
+
+
+
+
 
 
