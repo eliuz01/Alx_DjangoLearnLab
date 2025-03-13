cbfa76aad430b3528b62f5bdfb96ed9e50382c7e
ELIUZ <osongoeliuz14@gmail.com>
2025-03-13 22:31:20 +0300

    added validation error

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,10 +1,18 @@
 from .models import Book, Author
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:  #defines the behavior and settings for the serializer
         model = Book
         fields = '__all__'
+    
+    def validate_publicaion_year(self, data):
+        current_year = datetime.now().year
+        if data > current_year:
+            raise serializers.ValidationError(f"the publication error cannot be in the future")
+        return data
     
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
