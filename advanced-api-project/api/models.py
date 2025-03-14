0578b3fe8d43c747241a104890ea8db60b904363
ELIUZ <osongoeliuz14@gmail.com>
2025-03-14 12:13:10 +0300

    added comments

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Author(models.Model):
-    name = models.CharField(("Name"), max_length=100)
+class Author(models.Model): #creates model author that represents a table in database
+    name = models.CharField(("Name"), max_length=100) #created the field "name", which will be a column in the database table calledauthor
 
 
-class Book(models.Model):
+class Book(models.Model):#creates model Book that represents a table in database
     title = models.CharField(("Title"), max_length=100)
     publication_year = models.IntegerField(("Publication Date"))
     author = models.ForeignKey("Author", on_delete=models.CASCADE)
+'''
+The fields title, publication_year, and author will be columns in the database table called Books
+'''
     
--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -5,20 +5,20 @@ from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:  #defines the behavior and settings for the serializer
-        model = Book
-        fields = '__all__'
+        model = Book #the database table with data to be serialized 
+        fields = '__all__' #the data fields to be serialized in the table Book
     
-    def validate_publicaion_year(self, data):
-        current_year = datetime.now().year
+    def validate_publication_year(self, data):#the validate_field() method takes the field tame followed by two arguents, i.e. instance and data representing the field
+        current_year = datetime.now().year#declares a variable that takes the value of urrent year
         if data > current_year:
             raise serializers.ValidationError(f"the publication error cannot be in the future")
         return data
     
 class AuthorSerializer(serializers.ModelSerializer):
     related_books = BookSerializer["(many=True, read_only=True)"]
-    class Meta:
-        model  = Author 
-        fields = ("name", "related_books")
+    class Meta:#defines the behavior and settings for the serializer
+        model  = Author #the database table with data to be serialized 
+        fields = ("name", "related_books") #the data fields to be serialized in the table Author
     
   
 
