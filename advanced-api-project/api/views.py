6cd759be4041bf294ab4d4d5c6ab7e32de54ba86
ELIUZ <osongoeliuz14@gmail.com>
2025-03-16 22:38:47 +0300

    filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters import rest_framework as filters
 from .serializers import BookSerializer, AuthorSerializer
 from .models import Book, Author
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from django_filters import rest_framework as filters
-from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Custom filter for Book model
 class BookFilter(filters.FilterSet):
@@ -22,32 +22,35 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = (filters.DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter, OrderingFilter)  # Add OrderingFilter here
     filterset_class = BookFilter
     search_fields = ['title', 'author__name']  # Allow searching by title and author name
     ordering_fields = ['title', 'publication_year']  # Allow ordering by title and publication year
     ordering = ['title']  # Default ordering by title
 
-class BookDetailView(generics.DetailAPIView): 
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-     lookup_field = 'id'
-     permission_classes = [IsAuthenticatedOrReadOnly]
+# The other views remain the same as they handle create, update, delete, and retrieve
+class BookDetailView(generics.DetailAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'id'
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
-class BookCreateView(generics.CreateAPIView): 
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-     permission_classes = [IsAuthenticated]
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
-class BookUpdateView(generics.UpdateAPIView): 
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-     permission_classes = [IsAuthenticated]
-     lookup_field = 'id'
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'id'
+
 
 class BookDeleteView(generics.DestroyAPIView):
-     queryset = Book.objects.all()
-     serializer_class = BookSerializer
-     permission_classes = [IsAuthenticated]
-     lookup_field = 'id'
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_field = 'id'
