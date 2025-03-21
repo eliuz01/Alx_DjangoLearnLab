23b3d32e6d5e2285887ed5f5789c6c549246c290
ELIUZ <osongoeliuz14@gmail.com>
2025-03-21 19:41:19 +0300

    updated delete and update urls to indicate pk

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('register/', views.register, name='register.html'),
     path('profile/', views.user_profile, name='profile.html'),    
     path('post_list/', PostListView.as_view(), name='post_list.html'),
-    path('post_detail/', PostDetailView.as_view(), name='post_detail.html')
-    path('post_create/', PostCreateView.as_view(), name='post_create.html')
-    path('post_update/', PostUpdateView.as_view(), name='post_update.html')
-    path('post_delete/', PostDeleteView.as_view(), name='post_delete.html')
+    path('post_detail/', PostDetailView.as_view(), name='post_detail.html'),
+    path('post/new/', PostCreateView.as_view(), name='post_create.html'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update.html'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete.html'),
 ]
 
