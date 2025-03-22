1f7512e643f4a2ddca8202c929d6c32014db0449
ELIUZ <osongoeliuz14@gmail.com>
2025-03-22 04:48:41 +0300

    added LoginRequiredMixin, UserPassesTestMixin

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,8 @@ from .forms import UserProfileForm, UserUpdateForm
 from django.contrib import messages
 from .models import Post
 from django.views.generic.detail import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse
 
 # Create your views here.
 
@@ -27,10 +29,12 @@ def user_profile(request):
 def register(request):
     return render(request, 'register.html')
 
-class PostListView(ListView):
+class PostListView(LoginRequiredMixin, ListView):
+    login_url = 'login/'
+    redirect_field_name = 'login'
     model = Post
     template_name = 'blog/post_list.html'
-    context_object_name = 'post'
+    context_object_name = 'posts'
 
 class PostDetailView(DetailView):
     model = Post
@@ -42,10 +46,18 @@ class PostCreateView(CreateView):
     template_name = 'blog/post_create.html'
     context_object_name = 'post'
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'blog/post_update.html'
     context_object_name = 'post'
+
+    # Custom method that determines if the user can update the post
+    def test_func(self):
+        post = self.get_object()
+        return post.author == self.request.user
+    
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.pk})
 
 class PostDeleteView(DeleteView):
     model = Post
