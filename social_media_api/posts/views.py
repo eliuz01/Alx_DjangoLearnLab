caa7380883669c9d55a458651fa7f7ccffabf0e5
ELIUZ <osongoeliuz14@gmail.com>
2025-03-28 11:12:29 +0300

    created utls.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 # Create your views here.
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow the author of the post/comment to edit or delete it.
+    """
+    def has_object_permission(self, request, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the author of the post/comment
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    
+    def perform_create(self, serializer):
+        # Automatically set the author to the currently authenticated user
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        # Optionally filter posts for a specific user (e.g., for a profile view)
+        user = self.request.user
+        return Post.objects.filter(author=user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Automatically set the author to the currently authenticated user
+        post = serializer.validated_data['post']
+        if not post:
+            raise PermissionDenied("Comment must be associated with a valid post.")
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        # Optionally filter comments for a specific user or post
+        post_id = self.request.query_params.get('post_id')
+        if post_id:
+            return Comment.objects.filter(post_id=post_id)
+        return Comment.objects.all()
